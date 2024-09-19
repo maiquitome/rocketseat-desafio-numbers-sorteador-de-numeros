e4749a3cfecf481152e29cd613960c7bc72a6461
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-19 15:27:16 -0300

    feat: :sparkles: add number-draw-results

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("JavaScript connected!");
+const numberDraw = document.getElementById("number-draw");
+const numberDrawResults = document.getElementById("number-draw-results");
+
+const numberDrawButton = numberDraw.querySelector("button");
+
+// numberDraw.style.display = "none";
+// numberDrawResults.style.display = "flex";
+
+numberDrawButton.onclick = (event) => {
+  event.preventDefault();
+
+  numberDraw.style.display = "none";
+  numberDrawResults.style.display = "flex";
+};
